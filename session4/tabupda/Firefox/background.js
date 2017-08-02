4a64d508c357056c45df990bbcbe8cc7da10f860
viswa <shihan.viswa@gmail.com>
2017-08-02 09:43:35 +0530

    tab update

---
+++ session4/tabupda/Firefox/background.js
@@ -0,0 +1,18 @@
+
+function handleCreated(tab) {
+  console.log("the created tab id is "+tab.id);
+
+var updating = browser.tabs.update({url: "https://developer.mozilla.org"});
+updating.then(onUpdated, onError);
+}
+
+browser.tabs.onCreated.addListener(handleCreated);
+
+function onUpdated(tab) {
+  console.log(`Updated tab: ${tab.id}`);
+}
+
+function onError(error) {
+  console.log(`Error: ${error}`);
+}
+
